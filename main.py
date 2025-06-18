179524bb8e20d2a80654c19bf9d8b1e01a9831a1
zern <makszarkij99@gmail.com>
2025-06-18 02:12:27 +0200

    player_xd

--- main.py
+++ main.py
@@ -5,6 +5,37 @@ import sys
 WIDH, HEIGH = 1000, 800
 
 
+class Player:
+    def __init__(self, frame, x=WIDH/2, y=HEIGH/2):
+        self.x = x
+        self.y = y
+
+        self.frame = frame
+        
+        self.speed = 10
+        self.size_player = 30
+        self.color = (255, 255, 255)
+
+    def handle_keyboard(self):
+        kb = pygame.key.get_pressed()
+        if kb[pygame.K_w]:
+            self.y -= self.speed
+            self.color = (155, 155, 255)
+        elif kb[pygame.K_s]:
+            self.y += self.speed
+            self.color = (155, 155, 255)
+        elif kb[pygame.K_d]:
+            self.x += self.speed
+            self.color = (155, 155, 255)
+        elif kb[pygame.K_a]:
+            self.x -= self.speed
+            self.color = (155, 155, 255)
+        else:
+            self.color = (255, 255, 255)
+    
+    def draw_player(self):
+        pygame.draw.circle(self.frame, self.color, (int(self.x), int(self.y)), self.size_player)
+
 class Game:
     def __init__(self):
         pygame.mixer.init()
@@ -19,7 +50,13 @@ class Game:
 
         self.running = True
         self.end_game = False
-    
+
+        self.player = Player(self.screen)
+
+    def handle_player(self, player):
+        player.handle_keyboard()
+        player.draw_player()
+
     def main(self):
         while self.running:
             try:
@@ -27,6 +64,12 @@ class Game:
                     if event.type == pygame.QUIT:
                         self.running = False
                         sys.exit()
+                self.screen.fill((20, 20, 20))
+
+                self.handle_player(self.player)
+
+                pygame.display.flip()
+                self.clock.tick(60)
             except Exception as e:
                 print(f"Error in main cycle: {e}")
 
